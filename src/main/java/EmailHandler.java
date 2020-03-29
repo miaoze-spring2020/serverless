47d840d9badb6676e7e5faf6f79fcb94522ca4b7
mzc <miao.ze@husky.neu.edu>
2020-03-29 16:05:32 -0400

    lambda

---
+++ src/main/java/EmailHandler.java
@@ -0,0 +1,53 @@
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class EmailHandler implements RequestHandler<SNSEvent, String> {
+
+    private static AWSCredential myCredential = new AWSCredential();
+
+    public String handleRequest(SNSEvent event, Context context) {
+        String record = event.getRecords().get(0).getSNS().getMessage();
+        JSONObject recordJson = new JSONObject(new JSONTokener(new JSONObject(record).toString()));
+
+        String username = recordJson.getString("username");
+        String bills = recordJson.getJSONArray("bills").toString();
+
+        DynamoDBService dbService = new DynamoDBService();
+        if (!dbService.existsItem(username)) {
+            sendEmail(username, bills);
+        }
+        return null;
+    }
+
+    private void sendEmail(String username, String message) {
+        try {
+            AmazonSimpleEmailService client =
+                    AmazonSimpleEmailServiceClientBuilder.standard()
+                            .withCredentials(new AWSStaticCredentialsProvider(myCredential.getCredentials()))
+                            .withRegion(myCredential.getRegion()).build();
+            SendEmailRequest request = new SendEmailRequest()
+                    .withDestination(
+                            new Destination().withToAddresses(username))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withHtml(new Content()
+                                            .withCharset("UTF-8").withData(message))
+                                    .withText(new Content()
+                                            .withCharset("UTF-8").withData(message)))
+                            .withSubject(new Content()
+                                    .withCharset("UTF-8").withData("Bills requested by you")))
+                    .withSource("prod.zechuanmiao.me");
+            //configuration set not used
+            client.sendEmail(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
